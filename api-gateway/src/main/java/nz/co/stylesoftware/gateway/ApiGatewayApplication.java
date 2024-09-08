7fa41657daa6fee075542f7998f7e40be16d4374
swati <swati.ralekar@gmail.com>
2024-09-09 11:02:32 +1200

    Added API Gateway to route

---
+++ api-gateway/src/main/java/nz/co/stylesoftware/gateway/ApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package nz.co.stylesoftware.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGatewayApplication.class, args);
+	}
+
+}
