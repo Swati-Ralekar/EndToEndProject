43ac9e636ecd1a875532ec48a24e5bc373ad044b
swati <swati.ralekar@gmail.com>
2024-09-12 15:36:37 +1200

    Aggregated Rest API Documentation

--- api-gateway/src/main/java/nz/co/stylesoftware/gateway/config/SecurityConfig.java
+++ api-gateway/src/main/java/nz/co/stylesoftware/gateway/config/SecurityConfig.java
@@ -8,10 +8,14 @@ import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
-	
+
+	private final String[] freeResourceUrls = {"/swagger-ui.html","/swagger-ui/**","/api-docs/**",
+	"/v3/api-docs/**","/swagger-resources/**","/aggregate/**"};
+
 	@Bean
 	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-		return http.authorizeHttpRequests(authorize -> authorize.anyRequest()
+		return http.authorizeHttpRequests(authorize -> authorize
+						.requestMatchers(freeResourceUrls).permitAll().anyRequest()
 				.authenticated())
 		        .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()))
 		        .build();
--- order-service/src/main/java/nz/co/stylesoftware/order/client/InventoryClient.java
+++ order-service/src/main/java/nz/co/stylesoftware/order/client/InventoryClient.java
@@ -1,13 +1,10 @@
 package nz.co.stylesoftware.order.client;
 
-import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.service.annotation.GetExchange;
 
-@FeignClient(value = "inventory", url="${inventory.url}")
 public interface InventoryClient {
 
-	@RequestMapping(value = "/api/inventory", method = RequestMethod.GET)
+	@GetExchange("/api/inventory")
 	boolean isInStock(@RequestParam String skuCode, @RequestParam Integer quantity);
 }
